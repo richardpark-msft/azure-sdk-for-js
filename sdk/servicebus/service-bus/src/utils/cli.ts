61fd83c290475f8b0e602fa49f29218720bf9af9
Richard Park <51494936+richardpark-msft@users.noreply.github.com>
2021-02-13 08:43:37 +0000

    Missed some changes.

--- sdk/servicebus/service-bus/src/utils/cli.ts
+++ sdk/servicebus/service-bus/src/utils/cli.ts
@@ -7,24 +7,26 @@ import { readFileSync } from "fs";
  * @internal
  */
 export function loadArgFromFileOrText(arg: string): { body: string } {
-    let text = arg;
-    if (arg[0] === "@") {
-        text = readFileSync(arg.slice(1)).toString();
+  let text = arg;
+  if (arg[0] === "@") {
+    text = readFileSync(arg.slice(1)).toString();
+  }
+
+  // is the text actually a serialized ServiceBusMessage?
+  try {
+    const possibleMessage = /** @type {import("@azure/service-bus").ServiceBusMessage} */ JSON.parse(
+      text
+    );
+
+    if (possibleMessage.body != null) {
+      // let's just assume it is a service bus message
+      return possibleMessage;
     }
+  } catch (_err) {}
 
-    // is the text actually a serialized ServiceBusMessage?
-    try {
-        const possibleMessage = /** @type {import("@azure/service-bus").ServiceBusMessage} */(JSON.parse(text));
-
-        if (possibleMessage.body != null) {
-            // let's just assume it is a service bus message
-            return possibleMessage;
-        }
-    } catch (_err) {}
-
-    // if there are JSON related errors or it doesn't have a 'body' field let's just assume it's text and 
-    // just send it in the body.
-    return {
-        body: text
-    };
+  // if there are JSON related errors or it doesn't have a 'body' field let's just assume it's text and
+  // just send it in the body.
+  return {
+    body: text
+  };
 }
